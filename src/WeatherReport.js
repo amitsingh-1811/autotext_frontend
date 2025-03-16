d6fada7867f82aba8042cde8a8568fa2ec730d51
amitsingh-1811 <78679332+amitsingh-1811@users.noreply.github.com>
2025-03-16 14:47:11 +0530

    Update WeatherReport.js

--- src/WeatherReport.js
+++ src/WeatherReport.js
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+import { useState } from 'react';
 import './WeatherReport.css';
 import ShowData from './ShowData';
 function WeatherReport({cityName}){
